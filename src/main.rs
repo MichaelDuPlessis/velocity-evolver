ca3aeda8d321e33cfd6ddd24f52ac8bc2073c743
Michael du Plessis <mdp0101@gmail.com>
2023-09-04 11:14:55 +0200

    tested quality of solution

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::ops;
-
 use mikes_ge::{ge::GE, grammer::Grammer};
-use mikes_pso::{particle::Particle, pso::PSO, vector::Vector, VelocityFunction};
+use mikes_pso::{particle::Particle, pso::PSO, vector::Vector};
+use rand::Rng;
 
+#[derive(Debug)]
 enum Velocity {
     Current,
     Best,
     Mul(Box<Self>, Box<ScalarOps>),
     Add(Box<Self>, Box<Self>),
+    Sub(Box<Self>, Box<Self>),
 }
 
+#[derive(Debug)]
 enum ScalarOps {
     Cognitive,
     Social,
@@ -17,6 +19,7 @@ enum ScalarOps {
     Rand,
     Mul(Box<ScalarOps>, Box<ScalarOps>),
     Add(Box<ScalarOps>, Box<ScalarOps>),
+    Sub(Box<ScalarOps>, Box<ScalarOps>),
 }
 
 impl Grammer for Velocity {
@@ -24,7 +27,9 @@ impl Grammer for Velocity {
     type Output = f64;
 
     fn run(&self, _input: &Self::Input) -> Self::Output {
-        let obj_func = |_coords: &Vector<2>| 7.0;
+        let obj_func = |coords: &Vector<2>| {
+            0.26 * (coords[0] * coords[0] + coords[1] * coords[1]) - 0.48 * coords[0] * coords[1]
+        };
         let func = |current: &_, best: &_| self.runner(current, best);
         let mut pso = PSO::new(100, [(-10.0, 10.0), (-10.0, 10.0)], func);
         let particle = pso.optimize(100, obj_func);
@@ -43,12 +48,13 @@ impl Velocity {
             Velocity::Best => best.coordinates(),
             Velocity::Mul(x, y) => x.runner(current, best) * y.runner(),
             Velocity::Add(x, y) => x.runner(current, best) + y.runner(current, best),
+            Velocity::Sub(x, y) => x.runner(current, best) - y.runner(current, best),
         }
     }
 
     fn generate_helper(pos: &mut usize, chromosome: &[u8]) -> Self {
         let p = *pos % chromosome.len();
-        let modulos = if *pos / chromosome.len() > 3 { 2 } else { 4 };
+        let modulos = if *pos / chromosome.len() > 3 { 2 } else { 5 };
         *pos += 1;
         match chromosome[p] % modulos {
             0 => Self::Current,
@@ -58,6 +64,10 @@ impl Velocity {
                 Box::new(ScalarOps::generate_helper(pos, chromosome)),
             ),
             3 => Self::Add(
+                Box::new(Self::generate_helper(pos, chromosome)),
+                Box::new(Self::generate_helper(pos, chromosome)),
+            ),
+            4 => Self::Sub(
                 Box::new(Self::generate_helper(pos, chromosome)),
                 Box::new(Self::generate_helper(pos, chromosome)),
             ),
@@ -72,15 +82,16 @@ impl ScalarOps {
             ScalarOps::Cognitive => 0.3,
             ScalarOps::Social => 0.3,
             ScalarOps::InertiaWeight => 0.5,
-            ScalarOps::Rand => 0.5,
+            ScalarOps::Rand => rand::thread_rng().gen(),
             ScalarOps::Mul(x, y) => x.runner() * y.runner(),
             ScalarOps::Add(x, y) => x.runner() + y.runner(),
+            ScalarOps::Sub(x, y) => x.runner() - y.runner(),
         }
     }
 
     fn generate_helper(pos: &mut usize, chromosome: &[u8]) -> Self {
         let p = *pos % chromosome.len();
-        let modulos = if *pos / chromosome.len() > 3 { 4 } else { 6 };
+        let modulos = if *pos / chromosome.len() > 3 { 4 } else { 7 };
         *pos += 1;
         match chromosome[p] % modulos {
             0 => Self::Rand,
@@ -95,12 +106,36 @@ impl ScalarOps {
                 Box::new(Self::generate_helper(pos, chromosome)),
                 Box::new(Self::generate_helper(pos, chromosome)),
             ),
+            6 => Self::Sub(
+                Box::new(Self::generate_helper(pos, chromosome)),
+                Box::new(Self::generate_helper(pos, chromosome)),
+            ),
             _ => panic!("Cannot get here"),
         }
     }
 }
 
 fn main() {
-    let mut ge = GE::<(), f64, Velocity>::new(100, (0.5, 0.5, 0.0), 3, 7, 100, 4, 1, &[((), 7.0)]);
-    ge.start();
+    let mut ge = GE::<(), f64, Velocity>::new(100, (0.5, 0.5, 0.0), 3, 7, 100, 4, 1, &[((), 0.0)]);
+    let chromosome = ge.start();
+    let velocity = Velocity::generate(&chromosome);
+    dbg!(&velocity);
+    let func = |current: &_, best: &_| velocity.runner(current, best);
+    let obj_func = |coords: &Vector<2>| {
+        0.26 * (coords[0] * coords[0] + coords[1] * coords[1]) - 0.48 * coords[0] * coords[1]
+    };
+    let mut pso = PSO::new(100, [(-10.0, 10.0), (-10.0, 10.0)], func);
+    let particle = pso.optimize(100, obj_func);
+    println!("Evolved Equation");
+    println!("{:?}", particle);
+    println!("{:?}", obj_func(&particle.coordinates()));
+    println!("Canonical Equation");
+    let mut pso = PSO::new(
+        100,
+        [(-10.0, 10.0), (-10.0, 10.0)],
+        mikes_pso::canonical_velocity,
+    );
+    let particle = pso.optimize(100, obj_func);
+    println!("{:?}", particle);
+    println!("{:?}", obj_func(&particle.coordinates()));
 }
